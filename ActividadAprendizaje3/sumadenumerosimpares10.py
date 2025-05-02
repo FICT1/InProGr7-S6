3b0a6d8fb380681ae3d38c1a5f7375ff7a5923fa
FICT1 <ficallejas@uamv.edu.ni>
2025-05-01 20:04:39 -0600

    No completado

---
+++ ActividadAprendizaje3/sumadenumerosimpares10.py
@@ -0,0 +1,15 @@
+# Ejercicio 10: Suma de números impares
+# Tipo de bucle: mientras
+# Enunciado: Calcula y muestra la suma de los números impares del 1 al 100.
+# Procedimiento paso a paso:
+# 1. Inicializa un contador en 1 y una suma en 0.
+# 2. Usa un bucle mientras que se ejecute mientras el contador sea menor o igual a 100.
+# 3. En cada iteración, si el número es impar, súmalo a la variable suma.
+# 4. Incrementa el contador en 1.
+# 5. Al final, muestra la suma total.
+
+print("**Suma de números impares**")
+import os
+os.system("cls || clear")
+contador = 1  
+
